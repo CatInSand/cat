929a98b0e99a2f40f166efdf32b683d9c4582528
FinnVerbeeck <239849759+CatInSand@users.noreply.github.com>
2026-05-21 18:17:58 +0200

    Created hash functions and literal overload

--- include/hash.h
+++ include/hash.h
@@ -1,9 +1,46 @@
 #ifndef CAT_HASH_H
 #define CAT_HASH_H
 
+#include <cassert>
+#include <string>
+
 namespace cat
 {
+	/*
+	Type used to store string hashes
+	*/
+	using hash_t = uint32_t;
 
+	/*
+	Creates a hash from a c_string using the sdbm hash
+	*/
+	consteval hash_t sdbm_hash(const char* string, size_t length)
+	{
+		hash_t result{ 0 };
+
+		for (size_t charIndex{ 0 }; charIndex < length; ++charIndex)
+		{
+			result = string[charIndex] + (result << 6) + (result << 16) - result;
+		}
+
+		return result;
+	}
+
+	/*
+	Creates a hash from a string using the sdbm hash
+	*/
+	consteval hash_t sdbm_hash(const std::string& string)
+	{
+		return sdbm_hash(string.c_str(), string.size());
+	}
+
+	namespace hash_literals
+	{
+		consteval cat::hash_t operator""_h(const char* string, size_t length)
+		{
+			return cat::sdbm_hash(string, length);
+		}
+	}
 }
 
 #endif
